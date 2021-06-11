4f1717f415edc53b2232fea9255815545855a5af
nguyenthanhliem-ntl <nguyenthanhlimpk@gmail.com>
2021-06-11 16:08:39 +0700

    up

---
+++ src/main/java/controller/TimeController.java
@@ -0,0 +1,36 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+import java.util.TimeZone;
+
+@Controller
+public class TimeController {
+    @GetMapping("/work")
+    public String happy(ModelMap model, @RequestParam (name = "city", required = false, defaultValue = "Asia/Ho_Chi_Minh") String city) {
+        Date date = new Date();
+        TimeZone local = TimeZone.getDefault();
+        TimeZone locale = TimeZone.getTimeZone(city);
+        long locale_time = date.getTime() +   (locale.getRawOffset() - local.getRawOffset());
+        date.setTime(locale_time);
+        model.addAttribute("city", city);
+        model.addAttribute("date", date);
+        return "index";
+    }
+
+    @GetMapping("/world-clock")
+    public String result(ModelMap model, @RequestParam (name = "city", required = false, defaultValue = "Asia/Ho_Chi_Minh") String city) {
+        Date date = new Date();
+        TimeZone local = TimeZone.getDefault();
+        TimeZone locale = TimeZone.getTimeZone(city);
+        long locale_time = date.getTime() +   (locale.getRawOffset() - local.getRawOffset());
+        date.setTime(locale_time);
+        model.addAttribute("city", city);
+        model.addAttribute("date", date);
+        return "index";
+    }
+}
